0906ad85ac8da8965b9223c6e2ab1dc42d4e0c4e
Zackery Ayscue <ztayscue@gmail.com>
2022-04-23 12:56:01 -0400

    Got k8s, helm, and skaffold setup.

--- src/music-catalog/main.go
+++ src/music-catalog/main.go
@@ -28,6 +28,10 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	e.GET("/", func(c echo.Context) error {
+		return c.HTML(http.StatusOK, "Hello, Docker! <3")
+	})
+
 	e.GET("/ping", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct{ Status string}{Status: "OK"})
 	})
